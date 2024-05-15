ff72ac5dac6c52655c5cdeca5c8e2bccbaa6e8d7
Sakib <ssar96540@gmail.com>
2024-05-15 12:12:43 +0600

    Added a comment & i don't know

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ require('dotenv').config()
 app.use(cors());
 app.use(express.json());
  
-
-
-
+// mongo db connection url
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rriax4f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -90,7 +88,7 @@ app.put('/spots/:id', async(req,res)=> {
   const result = await country.toArray();
   res.send(result)
  })
- app.get('/countryes/:country', async(req,res)=> {
+ app.get('/countryes/:country', async(req,res) => {
   const country = req.params.country;
   const query = {country : country};
   const result = await spotsCollection.find(query).toArray();
